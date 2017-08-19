1e12e18cbe8bbe3c890ad2ec577a8aced8c8613b
wafo <chancelin85@gmail.com>
2017-08-19 08:06:03 +0100

    ajout du fichier Readme au depot

---
+++ jcr_root/apps/data/login.js
@@ -0,0 +1,8 @@
+use(function () {
+    return {
+        company: ' Compagny SARL',
+        titreConnect: 'Connectez-vous sur APPS',
+        savePaswrd: ' Conserver votre mot de passe ? ',
+        connect: ' Connecter ',
+    };
+});
